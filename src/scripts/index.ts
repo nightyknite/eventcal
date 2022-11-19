485a56e64aa7dd43fcfdf7677d84b58c5378da86
nightyknite <nightyknite3@gmail.com>
2022-11-19 19:44:03 +0900

    ライブラリバージョンアップ対応

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,14 +2,39 @@ import * as bootstrap from 'bootstrap'
 import $ from 'jquery';
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import dayjs from 'dayjs'; 
+import tippy from 'tippy.js';
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
 
-const setCalendar = (start, callback) => {
-    let events = [];
-    let dt = new Date(start);
+const connpass = (data: { events: any[]; }) => {
+  return data.events.map(item => {
+    return {
+      title: `${item.title} (${item.limit})`,
+      start: item.started_at,
+      end: item.ended_at,
+      url: item.event_url,
+      limit: item.limit,
+      description: ""
+                    + "day:" + dayjs(item.started_at).format("MM/DD HH:mm") + " - "
+                    + "" + dayjs(item.ended_at).format("MM/DD HH:mm") + "<br>"
+                    + "limit:" + item.limit + "<br>"
+                    + "place:" + item.place + "<br>"
+                    + "address:" + item.address + "<br>"
+                    + "description:" + (item.catch ? item.catch.substring(0,49) : "") + "<br>"
+                    + "",
+      backgroundColor: '#a82400',
+      borderColor: '#a82400',
+      textColor: 'white'
+    }    
+  });
+}
+
+const setCalendar = (startStr: any, callback: any) => {
+    let events: any[] = [];
+    let dt = new Date(startStr);
     
     dt.setDate(dt.getDate() + 7);
 
@@ -19,98 +44,74 @@ const setCalendar = (start, callback) => {
     if (item !== null) {
       events = JSON.parse(item);
       const start = document.getElementById("start");
-      if (start.value.length > 0) {
+      if (start!.value.length > 0) {
         events = events.filter(event => {return (dayjs(event.start).format("HH:mm") >= start.value);});
       }
       const limit = document.getElementById("limit");
-      if (limit.value.length > 0) {
+      if (limit!.value.length > 0) {
         events = events.filter(event => {return (Number(event.limit) >= limit.value);});
       }
       const keyword = document.getElementById("keyword");
-      if (keyword.value.length > 0) {
+      if (keyword!.value.length > 0) {
         events = events.filter(event => {return (event.description.indexOf(keyword.value) > 0);});
       }
       callback(events);
       return;
     }
     
-    const connpass = data => {
-      let event = [];
-      for (var i in data.events) {
-        event.push({
-          title: data.events[i].title + '(' + data.events[i].limit + ')',
-          start: data.events[i].started_at,
-          end: data.events[i].ended_at,
-          url: data.events[i].event_url,
-          limit: data.events[i].limit,
-          description: ""
-                       + "day:" + dayjs(data.events[i].started_at).format("MM/DD HH:mm") + " - "
-                       + "" + dayjs(data.events[i].ended_at).format("MM/DD HH:mm") + "<br>"
-                       + "limit:" + data.events[i].limit + "<br>"
-                       + "place:" + data.events[i].place + "<br>"
-                       + "address:" + data.events[i].address + "<br>"
-                       + "description:" + (data.events[i].catch ? data.events[i].catch.substring(0,49) : "") + "<br>"
-                       + "",
-
-          backgroundColor: '#a82400',
-          borderColor: '#a82400',
-          textColor: 'white'
-        });
-      }
-      return event;
-    }
-
     (async () => {
         let data = [];
         let event = [];
         const results = [];
         const conpassTimes = 10;
-        const totalTimes = conpassTimes;
-        const progressArea = document.querySelector("#eventloading");
-        progressArea.max = totalTimes;
-        progressArea.style.display = 'block';
-        progressArea.value = 0;
-
         for (let i = 0; i < conpassTimes; i++) {
             results.push((async () => {
             data = await $.ajax({url: 'https://connpass.com/api/v1/event/?count=100&ym=' + ym + '&start=' + (i * 100 + 1), dataType: 'jsonp'});
             event = connpass(data);
             events = events.concat(event);
-            progressArea.value += 1;
             })());
         }
         await Promise.all(results);
-        progressArea.style.display = "none";
         sessionStorage.setItem('event' + ym, JSON.stringify(events));
         callback(events);
-
     })();
-
   }
-
-    const calendarEl = document.getElementById('calendar');
-    const calendar = new Calendar(calendarEl, {
-      plugins: [ dayGridPlugin, listPlugin ],
-      headerToolbar: { center: 'dayGridMonth,timeGridWeek' },
-      views: {
-        listDay: { buttonText: '日' },
-        listWeek: { buttonText: '週' }
-      },
-      locale: 'ja',
-      events: (info, successCallback) => setCalendar(info.startStr, successCallback),
-      eventClick: function(info) {
-        info.jsEvent.preventDefault();
-        if (info.event.url) {
-          window.open(info.event.url);
-        }
+  
+  const calendarEl: HTMLElement = document.getElementById('calendar')!;
+  const calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin, listPlugin ],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,listDay'
+    },
+    buttonText: {
+      today: '今日',
+      month: '月',
+      list: 'リスト'
+    },
+    locale: 'ja',
+    events: (info: any, success: any) => setCalendar(info.start.valueOf(), success),
+    eventClick: (info: any) => {
+      info.jsEvent.preventDefault();
+      if (info.event.url) {
+        window.open(info.event.url);
       }
-    });
+    },
+    eventDidMount: (e: any) => {
+      tippy(e.el, {
+        content: e.event.extendedProps.description,
+        allowHTML: true,
+      });
+    }
+  });
 
-    calendar.render();
+  calendar.render();
 
-    document.getElementById("search").addEventListener('click', ()=> {
-        calendar.refetchEvents();
-        calendar.render();
-    });
+  document.getElementById("search")!.addEventListener('click', ()=> {
+      calendar.refetchEvents();
+      calendar.render();
+  });
   
 });
